2c2e1af654b00ca1052fedfbb1cb534476e5c9a1
Patrick Schoenfeld <patrick.schoenfeld@credativ.de>
2012-07-04 12:09:37 +0200

    Initial import

---
+++ manifests/init.pp
@@ -0,0 +1,29 @@
+class apt_conf (
+    $ensure        = params_lookup('ensure'),
+    $use_backports = params_lookup('use_backports')
+    ) inherits apt_conf::params {
+
+    Class['apt'] -> Class['apt_conf']
+
+    apt::conf { 'apt_norecommends':
+        ensure      => $ensure,
+        priority    => '99',
+        content     => 'APT::Install-Recommends \'0\';'
+    }
+    apt::conf { 'apt_periodic-updates':
+        ensure      => $ensure,
+        priority    => '99',
+        content     => 'APT::Periodic::Update-Package-Lists 1;'
+    }
+
+    if $use_backports {
+        apt::source { 'debian_backports':
+            ensure              => $ensure,
+            location            => 'http://backports.debian.org/debian-backports/',
+            release             => 'squeeze-backports',
+            repos               => 'main contrib non-free',
+            required_packages   => 'debian-keyring debian-archive-keyring',
+            include_src         => false
+        }
+    }
+}
---
+++ manifests/params.pp
@@ -0,0 +1,4 @@
+class apt_conf::params {
+    $use_backports  = true
+    $ensure         = present
+}
